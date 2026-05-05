588ab30df34f1c85dfddd05c9763dafb7a64a40d
OtavioXavier <otavioxaviermarinho@gmail.com>
2026-05-05 15:19:59 -0300

    refactor: implementa controller pattern

--- src/app/controllers/customer-controller.js
+++ src/app/controllers/customer-controller.js
@@ -1,57 +1,55 @@
-class CustumerController {
+let customers = [
+  { id: 1, name: 'John', age: 30 },
+  { id: 2, name: 'Jane', age: 25 },
+  { id: 3, name: 'Bob', age: 40 },
+ ]
 
-  constructor() {
-    this.custumers = [
-      { id: 1, name: 'John', age: 30 },
-      { id: 2, name: 'Jane', age: 25 },
-      { id: 3, name: 'Bob', age: 40 },
-    ]
-  }
-
+class CustomerController {
+    
   index(req, res) {
-      return res.status(200).json(this.custumers)
+      return res.status(200).json(customers)
   }
 
-  
+
   show(req, res) {
     const id = parseInt(req.params.id)
-    const costumer = this.custumers.find(c => c.id === id)
+    const costumer = customers.find(c => c.id === id)
     const status = costumer ? 200 : 404
     return res.status(status).json(costumer ?? { error: true, message: 'Costumer not found' })
   }
 
-  
+
   create(req, res) {
     const { name, age } = req.body
-    const id = this.custumers[this.custumers.length - 1].id + 1 ?? 1
+    const id = customers[customers.length - 1].id + 1 ?? 1
     const newCostumer = { id, name, age }
-    this.custumers.push(newCostumer)
+    customers.push(newCostumer)
     return res.status(201).json(newCostumer)
   }
 
-  
+
   update(req, res) {
     const id = parseInt(req.params.id)
     const { name, age } = req.body
-    const index = this.custumers.findIndex(c => c.id === id);
+    const index = customers.findIndex(c => c.id === id);
     if (index === -1) return res.status(404).json({ error: true, message: 'Costumer not found' })
-    this.custumers[index] = {
+    customers[index] = {
       id,
       name,
       age,
     }
-    return res.status(200).json(this.custumers[index])
+    return res.status(200).json(customers[index])
   }
 
-  
+
   destroy(req, res) {
     const id = parseInt(req.params.id)
-    const index = this.custumers.findIndex(c => c.id === id);
+    const index = customers.findIndex(c => c.id === id);
     if (index === -1) return res.status(404).json({ error: true, message: 'Costumer not found' })
-    this.custumers.splice(index, 1)
+    customers.splice(index, 1)
     return res.status(200).json({ message: 'Costumer deleted' })
-    
+
   }
 }
 
-module.exports = new CustumerController();
+export default new CustomerController();
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
 import Router from 'express';
-import custumers from './app/controllers/custumer-controller';
+import customers from './app/controllers/customer-controller.js';
 const routes = Router();
 
-routes.get('/customers', custumers.index)
-routes.get('/customers/:id', custumers.show)
-routes.post('/customers', custumers.create)
-routes.put('/customers/:id', custumers.update)
-routes.delete('/customers/:id', custumers.destroy)
+routes.get('/customers', customers.index)
+routes.get('/customers/:id', customers.show)
+routes.post('/customers', customers.create)
+routes.put('/customers/:id', customers.update)
+routes.delete('/customers/:id', customers.destroy)
 
 
 export default routes;
